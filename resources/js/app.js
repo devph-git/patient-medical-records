55550ac60b20dcaa1b65f703f16485412b8a5380
Vincexx <cvpitagan@gmail.com>
2020-08-01 11:10:02 +0800

    added admin-dashboard template

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Vuetify from 'vuetify';
-Vue.use(Vuetify);git 
+
+Vue.use(Vuetify);
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+Vue.component('admin-navigation', require('./components/admin/Navigation.vue').default);
 
 
 const app = new Vue({
