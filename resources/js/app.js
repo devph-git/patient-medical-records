df4c6f655d640ed6e64dfa9afd6fffcadc2661c4
Vincexx <cvpitagan@gmail.com>
2020-08-01 11:39:08 +0800

    setup vue-router and navigation on admin-panel

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,29 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
 
 Vue.use(Vuetify);
+Vue.use(VueRouter);
 
+import Dashboard from './components/admin/Dashboard.vue';
+import User from './components/admin/User.vue';
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/admin/dashboard',
+            name: 'Dashboard',
+            component: Dashboard
+        },
+        {
+            path: '/admin/users',
+            name: 'User',
+            component: User
+        },
+    ],
+});
 
 Vue.component('admin-navigation', require('./components/admin/Navigation.vue').default);
 
@@ -17,4 +35,5 @@ Vue.component('admin-navigation', require('./components/admin/Navigation.vue').d
 const app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+    router,
 });
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard', function() {
-    return view('admin.dashboard');
-});
+// Route::get('admin/dashboard', function() {
+//     return view('admin.dashboard');
+// });
 
 Route::get('/admin/{any}', function () {
     return view('admin.dashboard');
