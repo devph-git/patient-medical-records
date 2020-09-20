2d277a8ee89eccebccb577cdb96493cadbfe552d
djma777 <jibe@djasuncion.com>
2020-09-20 18:49:19 +0800

    i forgot which one: "|" or "||"

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const IndexRouter = require("./routes/index.js");
 
 app.use(IndexRouter);
 
-app.listen(PORT | process.env.port, async () => {
+app.listen(PORT || process.env.port, async () => {
   console.log(`PMR-0 running at port ${PORT}`);
 });
